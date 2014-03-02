08f7ec209c6e1f865736828872f145fedfb9c2fd
Nick Newell <blotto@gmail.com>
2014-03-02 15:46:40 -0800

    really agressive removals of depends for now

--- lib/active_admin/parse_resource/helpers/collection.rb
+++ lib/active_admin/parse_resource/helpers/collection.rb
@@ -5,7 +5,7 @@ module ActiveAdmin
       alias original_collection_size collection_size
       original_collection_size = instance_method(:collection_size)
       def collection_size(collection=collection)
-        if(not collection.empty? and collection.first.class.included_modules.include?(ParseResource::Document))
+        if(not collection.empty? and collection.first.class.included_modules.include?(ParseResource::Base))
           collection.count(true)
         else
           original_collection_size(collection)
