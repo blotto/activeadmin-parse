53012af69805e4e70162b1b0c77d43d4ec472908
Nick Newell <blotto@gmail.com>
2014-03-02 16:00:28 -0800

    scoped

--- lib/active_admin/parse_resource/document.rb
+++ lib/active_admin/parse_resource/document.rb
@@ -136,6 +136,20 @@ module ActiveAdmin::ParseResource::Document
     def reflections *a
       relations *a
     end
+
+    def scoped(options = nil)
+      if options
+        scoped.apply_finder_options(options)
+      else
+        if current_scope
+          current_scope.clone
+        else
+          scope = relation.clone
+          scope.default_scoped = true
+          scope
+        end
+      end
+    end
   end
 end
 
